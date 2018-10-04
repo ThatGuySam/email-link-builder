d635a2801730e688c7bd66ac0e7ad865e18b0b1b
Sam Carlton <sam@sam.lc>
2018-10-03 19:24:30 -0500

    Lint main component

--- resources/assets/js/components/Main.vue
+++ resources/assets/js/components/Main.vue
@@ -1,6 +1,10 @@
 <template>
-  <div id="app" class="section">
-    <div class="container" style="max-width: 900px;">
+  <div
+    id="app"
+    class="section">
+    <div
+      class="container"
+      style="max-width: 900px;">
       <div class="columns">
         <div class="column animated fadeIn">
           <h2 class="subtitle">The Email</h2>
@@ -8,30 +12,30 @@
             <header class="card-header is-block">
               <div class="field">
                 <div class="control">
-                  <input 
+                  <input
+                    v-model="to"
                     class="input is-quiet is-medium"
-                    type="text" 
+                    type="text"
                     placeholder="To"
                     value=""
-                    v-model="to"
                     autofocus
                   >
-                    <!-- autofocus -->
+                  <!-- autofocus -->
                 </div>
                 <div class="control">
-                  <input 
-                    class="input is-quiet is-medium" 
-                    type="text" 
-                    placeholder="Cc"
+                  <input
                     v-model="cc"
+                    class="input is-quiet is-medium"
+                    type="text"
+                    placeholder="Cc"
                   >
                 </div>
                 <div class="control">
                   <input
+                    v-model="bcc"
                     class="input is-quiet is-medium"
                     type="email"
                     placeholder="Bcc"
-                    v-model="bcc"
                   >
                 </div>
               </div>
@@ -41,10 +45,10 @@
               <div class="field">
                 <div class="control">
                   <input
+                    v-model="subject"
                     class="input is-quiet is-medium"
                     type="text"
                     placeholder="Subject"
-                    v-model="subject"
                   >
                 </div>
               </div>
@@ -52,10 +56,10 @@
               <div class="field">
                 <div class="control">
                   <textarea
+                    v-model="body"
                     class="textarea is-quiet is-medium"
                     placeholder="Body"
-                    v-model="body"
-                  ></textarea>
+                  />
                 </div>
               </div>
 
@@ -63,31 +67,39 @@
             <footer class="card-footer">
               <p class="card-footer-item">
                 <span>
-                  <a :href="link" class="button is-medium is-primary" target="_blank">Send</a>
+                  <a
+                    :href="link"
+                    class="button is-medium is-primary"
+                    target="_blank">Send</a>
                 </span>
               </p>
             </footer>
           </div>
-          </div>
-          <div class="column">
-            <div v-if="to !== ''" class="animated fadeIn">
-              <h2 class="subtitle">The Link</h2>
-              <pre style="word-break: break-all;">
+        </div>
+        <div class="column">
+          <div
+            v-if="to !== ''"
+            class="animated fadeIn">
+            <h2 class="subtitle">The Link</h2>
+            <pre style="word-break: break-all;">
                 {{ link }}
-              </pre>
-              <br />
-              <h2 class="subtitle">The Html</h2>
-              <pre style="word-break: break-all;">
+            </pre>
+            <br >
+            <h2 class="subtitle">The Html</h2>
+            <pre style="word-break: break-all;">
                 {{ html }}
-              </pre>
-              <br />
-              <div class="level">
-                <p class="level-item has-text-centered">
-                  <a :href="link" class="button is-medium is-primary" target="_blank">Test</a>
-                </p>
-              </div>
+            </pre>
+            <br >
+            <div class="level">
+              <p class="level-item has-text-centered">
+                <a
+                  :href="link"
+                  class="button is-medium is-primary"
+                  target="_blank">Test</a>
+              </p>
             </div>
           </div>
+        </div>
       </div><!--  .columns  -->
     </div><!--  .container  -->
   </div><!--  .section  -->
@@ -97,7 +109,7 @@
   import is from 'is_js'
 
   export default {
-    data: function () {
+    data () {
       return {
         to: '',
         cc: '',
@@ -108,7 +120,7 @@
     },
     computed: {
       params () {
-        var params = {}
+        const params = {}
 
         if (is.not.empty(this.cc)) params.cc = this.cc
         if (is.not.empty(this.bcc)) params.bcc = this.bcc
@@ -118,30 +130,30 @@
         return params
       },
       queryString () {
-        var keys = Object.keys(this.params)
-        var query = keys.map(this.mapParam)
-        var queryString = query.join('&')
+        const keys = Object.keys(this.params)
+        const query = keys.map(this.mapParam)
+        const queryString = query.join('&')
 
         return queryString
       },
       link () {
         const toEncoded = encodeURIComponent(this.to)
-        let link = 'mailto:' + toEncoded
+        let link = `mailto:${toEncoded}`
 
         if (is.not.empty(this.queryString)) {
-          link += '?' + this.queryString
+          link += `?${this.queryString}`
         }
 
         return link
       },
       html () {
-        var linkText = is.not.empty(this.subject) ? this.subject : 'Say Hello'
-        return '<a href="' + this.link + '">' + linkText + '</a>'
+        const linkText = is.not.empty(this.subject) ? this.subject : 'Say Hello'
+        return `<a href="${this.link}">${linkText}</a>`
       }
     },
     methods: {
       mapParam (key) {
-        return encodeURIComponent(key) + '=' + encodeURIComponent(this.params[key])
+        return `${encodeURIComponent(key)}=${encodeURIComponent(this.params[key])}`
       }
     }
   }
