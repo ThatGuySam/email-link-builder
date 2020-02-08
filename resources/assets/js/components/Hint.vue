89ad046f96bb3fec59a27d31cb6cb23d2b0ef309
Sam Carlton <sam@sam.lc>
2020-02-07 18:35:42 -0600

    Add hint component

---
+++ resources/assets/js/components/Hint.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    style="all: unset;"
+    @click="isOpen = !isOpen"
+  >
+    <div
+      v-if="!isOpen"
+      class="icon has-background-light"
+      style="border-radius: 9999px;"
+    >?</div>
+    <slot v-else />
+  </button>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      isOpen: false
+    }
+  }
+}
+
+</script>
